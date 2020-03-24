43505c5fad635aaada681b31f04d6b36efdbfa2a
Maigal <matiasigalmeida@gmail.com>
2020-03-24 18:23:11 -0300

    server settings

--- server/index.js
+++ server/index.js
@@ -24,10 +24,10 @@ let mobUID = 0;
 //require('./db.js')
 
 // CLIENT TEMP WRAPPER 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '/../client')));
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'../client/index.html'));
+    res.sendFile(path.join(__dirname+'/../client/index.html'));
 });
 
 app.listen(80, () => {
