c1a616794558de74bc2f63834427833b1eb95666
Maigal <matiasigalmeida@gmail.com>
2020-03-29 16:59:49 -0300

    server changes

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ const helpers = require('./helpers')
 
 
 // CLIENT TEMP WRAPPER
-// const express = require('express')
-// const app = express();
-// const path = require('path');
+const express = require('express')
+const app = express();
+const path = require('path');
 // END CLIENT TEMP WRAPPER
 
 let state = require('./state.js')
@@ -25,15 +25,15 @@ let mobUID = 0;
 //require('./db.js')
 
 // CLIENT TEMP WRAPPER 
-// app.use(express.static(path.join(__dirname, '/../client')));
+app.use(express.static(path.join(__dirname, '/../client')));
 
-// app.get('*', (req,res) =>{
-//     res.sendFile(path.join(__dirname+'/../client/index.html'));
-// });
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/../client/index.html'));
+});
 
-// app.listen(80, () => {
-//     console.log('Listening on port 80!')
-// })
+app.listen(80, () => {
+    console.log('Listening on port 80!')
+})
 // END CLIENT TEMP WRAPPER 
 
 server.listen(PORT, (err) => {
